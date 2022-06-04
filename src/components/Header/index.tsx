7c4583d6857d205619ff18b8e93f352d85c9554f
Jean Michel <jeaanmichel@protonmail.com>
2022-06-04 20:18:49 -0300

    Created application header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import TodoLogo from "../../assets/img/todo-logo.png";
 
+import styles from "./Header.module.css";
+
 export const Header: React.FC = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={TodoLogo} alt="Todo app logo" />
     </header>
   );
