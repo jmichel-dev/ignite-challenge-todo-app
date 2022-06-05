aa4e9ddba006550f64b0312db220e37c7d191331
Jean Michel <jeaanmichel@protonmail.com>
2022-06-04 21:09:40 -0300

    created header for todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { ListTodo } from "./components/ListTodo";
 import { NewTodoForm } from "./components/NewTodoForm";
 import "./styles/global.css";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <NewTodoForm />
+      <ListTodo />
     </>
   );
 }
---
+++ src/components/ListTodo/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./ListTodo.module.css";
+
+export const ListTodo: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.content}>
+          <span className={styles.created}>Tarefas criadas</span>
+          <span className={styles.badge}>0</span>
+        </div>
+        <div className={styles.content}>
+          <span className={styles.done}>Tarefas concluídas</span>
+          <span className={styles.badge}>0</span>
+        </div>
+      </header>
+    </div>
+  );
+};
